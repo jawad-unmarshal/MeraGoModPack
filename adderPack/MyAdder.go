a1cbee5f00963fd1d8a40eb206ab3bf7cd2ac68f
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2021-08-18 12:10:03 +0530

    First commit

---
+++ adderPack/MyAdder.go
@@ -0,0 +1,5 @@
+package adderpack
+
+func Adder(a int, b int) int {
+	return a + b
+}
---
+++ powerPack/MyPower.go
@@ -0,0 +1,9 @@
+package powerpack
+
+func FindPow(base float64, raise int) float64 {
+	var pow float64 = 1.
+	for i := 1; i <= raise; i++ {
+		pow *= base
+	}
+	return pow
+}
